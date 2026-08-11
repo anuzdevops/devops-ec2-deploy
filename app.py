bd8aa0bd4672cd39a6155ce6497cd53bd8a77d9c
anuzdevops <108412361+anuzdevops@users.noreply.github.com>
2026-08-11 19:10:03 +0530

    Update app.py
    Added v2 instead of v1

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def home():
-    return "<h1>Deployed via DevOps Pipeline! v1</h1>"
+    return "<h1>Deployed via DevOps Pipeline! v2</h1>"
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
